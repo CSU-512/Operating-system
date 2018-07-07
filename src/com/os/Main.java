9e300d654a5c5b37d6cf93ef9582c2c2e5c5df3b
xyjiang <656385934@qq.com>
2018-07-08 01:04:07 +0800

    添加Base64 codec包

--- src/com/os/Main.java
+++ src/com/os/Main.java
@@ -3,12 +3,17 @@ package com.os;
 import com.externalStorage.ExternalStorage;
 import javafx.util.Pair;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import org.apache.commons.codec.binary.Base64;
+import sun.misc.BASE64Encoder;
+
 import java.util.Comparator;
+
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedEncodingException {
 	// write your code here
         System.out.println("haha");
         ArrayList<Pair<Integer, Integer>> arl = new ArrayList<>();
@@ -25,5 +30,9 @@ public class Main {
         for(Pair<Integer, Integer> p : arl)
             System.out.print(p.getKey() + " ");
         System.out.println();
+        String str = "haha";
+        System.out.println(Base64.encodeBase64String(str.getBytes("utf-8")));
+        System.out.println(new String(Base64.decodeBase64(Base64.encodeBase64String(str.getBytes("utf-8")))));
+        System.out.println(new String(Base64.decodeBase64("aGFoYQ==")));
     }
 }
