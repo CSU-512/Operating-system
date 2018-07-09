31366464a9af5b3ec6473f0da2ed88e79c438b50
Noah Sun <873717994@qq.com>
2018-07-09 15:19:53 +0800

    新建文件时设置了权限

--- src/com/util/JSONSaver.java
+++ src/com/util/JSONSaver.java
@@ -75,7 +75,7 @@ public class JSONSaver {
 
         //构建初始INode，即根结点信息
         ArrayList<INode> iNodes = new ArrayList<>();
-        iNodes.add(new INode(0, -1, "~", 0, FileTypeEnum.INODE_IS_DIRECTORY, "user1", "group1",
+        iNodes.add(new INode(0, -1, "~", 0, FileTypeEnum.INODE_IS_DIRECTORY, 0, 0,
                             0, new Date(), new Date(), new Date(), 0,
                             new ArrayList<>(), new HashMap<>()));//加入一个根结点
 
